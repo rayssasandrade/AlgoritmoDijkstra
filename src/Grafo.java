eea3e7d9f7fc31951ed5959e55486a354cb31caa
rayssa.andrade <rayssa.andrade071@outlook.com>
2021-08-26 22:42:47 -0300

    criado o grafo

---
+++ src/Grafo.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Grafo {
+
+    private List<Vertice> grafo = new ArrayList<Vertice>();
+
+    public List<Vertice> getVertices() {
+        return grafo;
+    }
+
+    public void addVertices(List<Vertice> grafo) {
+        this.grafo = grafo;
+    }
+
+    public Vertice getVerticesByName(String nome) {
+        for (int i = 0; i < this.getVertices().size(); i++) {
+            if (nome.equalsIgnoreCase(this.getVertices().get(i).getNome())) {
+                return this.getVertices().get(i);
+            }
+        }
+        return null;
+    }
+}
